e4938f4816c123c69a22b559d68504b5da89dd53
justin <thecrashtestjustin@gmail.com>
2022-10-24 07:59:51 -0700

    adds an if else statement to parse integers

--- script.js
+++ script.js
@@ -1,30 +1,70 @@
-gridSize = 100;
+const initialSize = 16;
+let newGridNumber;
+let gridSize;
 
 const container = document.querySelector('#container');
 const activateSizeChange = document.querySelector('.grid-size-change');
 const applySizeChange = document.querySelector('.activate-grid-size');
 const inputSizeChange = document.querySelector('.grid-size-input');
+const inputRejection = document.querySelector('.input-rejection');
 
 const gridHolder = document.createElement('div');
 gridHolder.classList.add('grid-holder');
 container.append(gridHolder);
 
+function setupGrid (size) {
+    gridHolder.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridHolder.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
-for (i = 0; i < 16; i++) {
-    for (j = 0; j < 16; j++) {
+for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
         const gridBlock = document.createElement('div');
         gridBlock.classList.add('grid-block');
         gridHolder.append(gridBlock);
     }
 }
+}
+
+//take value from inputsizechange
+//set new value as gridSize variable
+//run setupgrid with new value
+//clear value from input
+
+ function updateGridSize () {
+        console.log(newNumber);    
+    //gridHolder.innerHTML = '';
+        //setupGrid(gridSize);
+}
 
 activateSizeChange.addEventListener ('click', e => {
     applySizeChange.removeAttribute('disabled');
     inputSizeChange.removeAttribute('disabled');
+    inputRejection.textContent = '';
 })
 
 applySizeChange.addEventListener ('click', e => {
+    var newNumber = parseInt(inputSizeChange.value);
+    if (isNaN(newNumber)) {
+        inputRejection.textContent = "*Please enter a valid number"
+        applySizeChange.toggleAttribute('disabled');
+        inputSizeChange.toggleAttribute('disabled');
+        inputSizeChange.value = '';
+
+    } else if (newNumber > 64) {
+        inputRejection.textContent = "Please select a number less than 65"
+        applySizeChange.toggleAttribute('disabled');
+        inputSizeChange.toggleAttribute('disabled');
+        inputSizeChange.value = '';
+
+    } else {
+    // console.log(newNumber);
+    updateGridSize(newNumber);
     applySizeChange.toggleAttribute('disabled');
     inputSizeChange.toggleAttribute('disabled');
+    inputSizeChange.value = '';
+    }
 })
+
+window.onload = () => {
+    setupGrid(initialSize);
+}
